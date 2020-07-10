7f3452d920a8596e13ed7c4a1df0fe4237e17b93
Somto M.Ugeh <smugeh@gmail.com>
2020-07-10 21:50:48 +0100

    Mocking packages

---
+++ src/__tests__/hidden-message.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen, waitFor, queryByTestId } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { HiddenMessage } from '../hidden-message';
+
+// mock packages and its return values
+jest.mock('react-transition-group', () => {
+  return {
+    CSSTransition: (props) => (props.in ? props.children : null),
+  };
+});
+
+test('should show hidden message when toggle is clicked', async () => {
+  const message = 'Hello world';
+  render(<HiddenMessage>{message}</HiddenMessage>);
+
+  const toggleButton = screen.getByText(/toggle/i);
+  expect(screen.queryByText(/hello world/i)).not.toBeInTheDocument();
+
+  user.click(toggleButton);
+  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+
+  user.click(toggleButton);
+  expect(screen.queryByText(/hello world/i)).not.toBeInTheDocument();
+});

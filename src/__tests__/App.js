5d88216142dc72dfe4cfedf982a640cf72ad7d38
Somto M.Ugeh <smugeh@gmail.com>
2020-07-10 13:24:20 +0100

    .

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -4,11 +4,9 @@ import App from '../App';
 
 describe('Testing App', () => {
   test('should render the app', () => {
-    const { rerender } = render(<App />);
+    render(<App />);
 
     const learnReactText = screen.getByText(/learn/i);
     expect(learnReactText).toBeInTheDocument();
-
-    rerender(<App />);
   });
 });
---
+++ src/__tests__/favoriteNumber.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoriteNumber } from '../favoriteNumber';
+import user from '@testing-library/user-event';
+
+test('should render an input with label "Favorite Number"', () => {
+  const { rerender, debug } = render(<FavoriteNumber />);
+
+  const input = screen.getByLabelText(/favorite number/i);
+  expect(input).toBeInTheDocument();
+
+  user.type(input, '10');
+  const alert = screen.getByRole('alert');
+  expect(alert).toBeInTheDocument();
+  expect(alert).toHaveTextContent(/number is invalid/i);
+
+  rerender(<FavoriteNumber max={10} />);
+  expect(screen.queryByRole('alert')).toBeNull();
+
+  debug();
+});
